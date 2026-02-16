8cd3c2afa0224418b6ac98865c4032d5d3a6260d
zhengsz20 <zhengsz2000@gmail.com>
2026-02-16 15:05:54 +0800

    Update por.R

--- siAge/por.R
+++ siAge/por.R
@@ -1,1 +1,15 @@
+library(data.table)
+data <- fread("D:/HuaweiMoveData/Users/Zheng/Desktop/PIK3IP1_immue/SIAGE/pcor.txt")
+library(ppcor)
+library(fastDummies)
+
+data$BMI <- as.numeric(data$BMI)
+pcor_result <- pcor.test(
+  x = data$CD8_TCM_HAVCR2, 
+  y = data$siage, 
+  z = data$BMI, 
+  method = "spearman"
+)
+result <- as.data.frame(print(pcor_result))
+aresult <- result[,c(1,2,6)]
 
